c156bc8da8c1c45a4b0137ee9f37af50fd0a5358
gladsonchala <gladsonchala@gmail.com>
2024-11-06 06:50:58 +0300

    app.js added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'eact';
+import TerminalComponent from './components/TerminalComponent';
+
+function App() {
+  return (
+    <div>
+      <TerminalComponent />
+    </div>
+  );
+}
+
+export default App;
